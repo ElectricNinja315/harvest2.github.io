a0b3a338e21315c315f0d6eb343f8a12215a454c
Havesuto <Havesuto@gmail.com>
2019-07-23 09:48:06 -0700

    add css

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var slider = document.getElementById("myRange");
 var slider2 = document.getElementById("myRange2");
 var output = document.getElementById("demo");   // container of graph
 var output2 = document.getElementById("demo2");
-output.innerHTML = slider.value; // Display the default slider value
 
 
 // Update the current slider value (each time you drag the slider handle)
